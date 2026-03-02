f1df42a7164e7d4a93b3b333a55e5be6689f816a
thomasbaudouin05-code <thomasbaudouin05@gmail.com>
2026-03-02 16:25:20 +0100

    Launch-system file creation
    Création d'un fichier de lancement permettant de lancer simultanément les noeuds suivants du robot : automatic, console, control, detect, health, lidar.

---
+++ robot2_ws/robot2_ws/src/robot_nodes/launch/robot_launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    
+    # Liste des noms des scripts python à lancer
+    nodes_to_launch = [
+        'automatic',
+        'console',
+        'control',
+        'detect',
+        'health',
+        'lidar'
+    ]
+    
+    ld = LaunchDescription()
+    
+    # Ouverture des différents terminaux de type xterm
+    for node_name in nodes_to_launch :
+        node_action = Node(
+            package = 'robot_nodes',
+            executable = node_name,
+            name = node_name,
+            output = 'screen',
+            prefix = ['xterm -e'],
+            emulate_tty = True
+        )
+        
+        ld.add_action(node_action)
+    
+    return ld
