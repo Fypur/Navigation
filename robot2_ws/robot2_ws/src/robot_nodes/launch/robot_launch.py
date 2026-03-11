e42c0697b842aacc4bcbd83d0fdfa1c0ad087948
Fypur <84095553+Fypur@users.noreply.github.com>
2026-03-11 09:28:15 +0100

    using gnome-terminal instead of xterm

--- robot2_ws/robot2_ws/src/robot_nodes/launch/robot_launch.py
+++ robot2_ws/robot2_ws/src/robot_nodes/launch/robot_launch.py
@@ -3,30 +3,21 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    
+
     # Liste des noms des scripts python à lancer
-    nodes_to_launch = [
-        'automatic',
-        'console',
-        'control',
-        'detect',
-        'health',
-        'lidar'
-    ]
-    
+    nodes_to_launch = ['automatic', 'console', 'control', 'detect', 'health', 'lidar']
+
     ld = LaunchDescription()
-    
+
     # Ouverture des différents terminaux de type xterm
-    for node_name in nodes_to_launch :
-        node_action = Node(
-            package = 'robot_nodes',
-            executable = node_name,
-            name = node_name,
-            output = 'screen',
-            prefix = ['xterm -e'],
-            emulate_tty = True
-        )
-        
+    for node_name in nodes_to_launch:
+        node_action = Node(package='robot_nodes',
+                           executable=node_name,
+                           name=node_name,
+                           output='screen',
+                           prefix=['gnome-terminal -- '],
+                           emulate_tty=True)
+
         ld.add_action(node_action)
-    
+
     return ld
