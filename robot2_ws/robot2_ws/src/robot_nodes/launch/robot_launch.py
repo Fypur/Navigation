e5df23fc3626d82d5968be7e6a04c7d020f71a8b
Fypur <84095553+Fypur@users.noreply.github.com>
2026-03-16 14:26:33 +0100

    Separated simulation package

--- robot2_ws/robot2_ws/src/robot_nodes/launch/robot_launch.py
+++ robot2_ws/robot2_ws/src/robot_nodes/launch/robot_launch.py
@@ -2,22 +2,28 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 
+def launch_node_with_terminal(package_name: str, node_name: str):
+    return Node(package=package_name,
+                executable=node_name,
+                name=node_name,
+                output='screen',
+                prefix=['xterm -e '],
+                emulate_tty=True)
+
+
 def generate_launch_description():
 
     # Liste des noms des scripts python à lancer
-    nodes_to_launch = ["driver", 'automatic', 'console', 'control', 'detect', 'health', 'lidar']
+    robot_nodes_to_launch = ['automatic', 'console', 'control', 'detect', 'health', 'lidar']
+    #simulation_nodes_to_launch = ["driver"]
 
     ld = LaunchDescription()
 
     # Ouverture des différents terminaux de type xterm
-    for node_name in nodes_to_launch:
-        node_action = Node(package='robot_nodes',
-                           executable=node_name,
-                           name=node_name,
-                           output='screen',
-                           prefix=['xterm -e '],
-                           emulate_tty=True)
+    for node_name in robot_nodes_to_launch:
+        ld.add_action(launch_node_with_terminal("robot_nodes", node_name))
 
-        ld.add_action(node_action)
+    #for node_name in simulation_nodes_to_launch:
+    #    ld.add_action(launch_node_with_terminal("simulation", node_name))
 
     return ld
