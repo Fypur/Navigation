2b9ff7cf5006b4e78b885b78a2e136f5fd14052e
Fypur <84095553+Fypur@users.noreply.github.com>
2026-03-25 20:19:48 +0100

    Updated simulation and added steadynode to fix wsl2 issues

--- robot2_ws/robot2_ws/src/robot_nodes/robot_nodes/console.py
+++ robot2_ws/robot2_ws/src/robot_nodes/robot_nodes/console.py
@@ -1,28 +1,26 @@
 import rclpy
 import threading
 import queue
-from rclpy.node import Node
+from robot_nodes.steady_node import SteadyNode
 from robot_msgs.msg import Health, Command
 
+SPEED = 100
+TURN_ANGLE = 15.0
 
-class Console(Node):
+
+class Console(SteadyNode):
 
     def __init__(self):
         super().__init__("console")
         self.last_cmd = "forward"
 
-        self.pub_health = self.create_publisher(Health, "/robot/health", 10)
+        self.pub_health = self.create_publisher(Health, "/robot/health", 1)
 
-        self.pub_cmd = self.create_publisher(Command, "/robot/command", 30)
+        self.pub_cmd = self.create_publisher(Command, "/robot/command", 10)
 
         self.create_timer(0.1, self.heartbeat)
-        self.create_timer(0.05, self.process_queue)
 
-        # file de commandes thread-safe
-        self.cmd_queue = queue.Queue()
-
-        # thread clavier
-        threading.Thread(target=self.input_loop, daemon=True).start()
+        self.input_loop()
 
     def heartbeat(self):
         self.pub_health.publish(Health(state="Hello", name="console"))
@@ -40,40 +38,39 @@ class Console(Node):
         while rclpy.ok():
             self.show_menu()
             cmd = input("> ")
+
             if cmd == "":
                 cmd = self.last_cmd
             else:
                 self.last_cmd = cmd
-            self.cmd_queue.put(cmd)
 
-    def process_queue(self):
-        while not self.cmd_queue.empty():
+            self.process(cmd)
 
-            cmd = self.cmd_queue.get()
-            m = Command()
+    def process(self, cmd: str):
+        m = Command()
 
-            if cmd == "forward":
-                m.action = "speed"
-                m.arg1 = 10  #this represents speed, in a byte format
-                #TODO:  Change cmd line to take an argument in speed
-            elif cmd == "backward":
-                m.action = "speed"
-                m.arg1 = -10
-            elif cmd == "stop":
-                m.action = "speed"
-                m.arg1 = 0
-            elif cmd == "left":
-                m.action = "turn"
-                m.arg1 = -15.0  #turn angle
-            elif cmd == "right":
-                m.action = "turn"
-                m.arg1 = 15.0
-            else:
-                self.get_logger().info("Unknown command")
-                return
+        if cmd == "forward":
+            m.action = "speed"
+            m.arg1 = SPEED  #this represents speed, in a byte format
+            #TODO:  Change cmd line to take an argument in speed
+        elif cmd == "backward":
+            m.action = "speed"
+            m.arg1 = -SPEED
+        elif cmd == "stop":
+            m.action = "speed"
+            m.arg1 = 0
+        elif cmd == "left":
+            m.action = "turn"
+            m.arg1 = -TURN_ANGLE
+        elif cmd == "right":
+            m.action = "turn"
+            m.arg1 = TURN_ANGLE
+        else:
+            self.get_logger().info("Unknown command")
+            return
 
-            self.pub_cmd.publish(m)
-            #self.get_logger().info(f"Sent: {m.action} with arg1 {m.arg1}")
+        self.pub_cmd.publish(m)
+        #self.get_logger().info(f"Sent: {m.action} with arg1 {m.arg1}")
 
 
 def main():
