cff296e0854a25fde43cf04ba4214c4fe0d6611f
Fypur <84095553+Fypur@users.noreply.github.com>
2026-04-14 17:36:15 +0200

    Changed launch file not to use xterm and made console launched only in standalone

--- robot_ws/src/robot/launch/robot_launch.py
+++ robot_ws/src/robot/launch/robot_launch.py
@@ -9,7 +9,6 @@ def launch_node_with_terminal(package_name: str, node_name: str):
                 executable=node_name,
                 name=node_name,
                 output='screen',
-                prefix=[f'xterm -hold -e'],
                 
                 emulate_tty=True)
 
@@ -52,14 +51,13 @@ def generate_launch_description():
                        executable="serial",
                        name="serial",
                        output='screen',
-                       prefix=['xterm -hold -e '],
                        parameters=[{
                            "serial_port": LaunchConfiguration('arduino_serial_port'),
                        }],
                        emulate_tty=True)
 
     # Liste des noms des scripts python à lancer dans le package robot
-    robot_nodes_to_launch = ['console', 'control', 'lidar', 'encoders']
+    robot_nodes_to_launch = ['control', 'lidar', 'encoders']
     #simulation_nodes_to_launch = ["driver"]
 
     ld = LaunchDescription()
