4e1a3ee7addc441420fc82c4f6853763482f8086
thomasbaudouin05-code <thomasbaudouin05@gmail.com>
2026-05-12 22:49:05 +0200

    re modif obstacle_margin

--- robot_ws/src/robot/robot/automatic.py
+++ robot_ws/src/robot/robot/automatic.py
@@ -12,7 +12,7 @@ DWA_SIM_TIME        = 1.5     # s     durée de simulation d'une trajectoire
 DWA_SIM_STEPS       = 15      # int   pas de simulation
 
 DWA_ROBOT_RADIUS    = 0.15    # m     rayon du robot (clearance)
-DWA_OBSTACLE_MARGIN = 0.10    # m     marge de sécurité supplémentaire
+DWA_OBSTACLE_MARGIN = 0.5    # m     marge de sécurité supplémentaire
 DWA_LETHAL_DIST     = 0.10    # m     distance en dessous de laquelle on bloque
 
 DWA_W_HEADING       = 0.5     # poids cap vers le but
